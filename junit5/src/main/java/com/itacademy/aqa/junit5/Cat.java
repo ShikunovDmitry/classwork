f6c7cdaf39e43d06d92e9a342aff13b6f20f7d2b
Dzmitry.Shykunou <Dzmitry.Shykunou@cantire.com>
2024-05-14 19:09:19 +0400

    exception handler listener example

--- junit5/src/main/java/com/itacademy/aqa/junit5/Cat.java
+++ junit5/src/main/java/com/itacademy/aqa/junit5/Cat.java
@@ -2,7 +2,7 @@ package com.itacademy.aqa.junit5;
 
 public class Cat extends Animal{
     public String whoIam() {
-        if(true) throw new NullPointerException("Exception for Cat");
+//        if(true) throw new NullPointerException("Exception for Cat");
         return "Cat";
 
 
--- junit5/src/test/java/com/itacademy/aqa/junit5/ListenerExample.java
+++ junit5/src/test/java/com/itacademy/aqa/junit5/ListenerExample.java
@@ -1,13 +1,16 @@
 package com.itacademy.aqa.junit5;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.opentest4j.AssertionFailedError;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class ListenerExample implements TestWatcher, AfterAllCallback {
+public class ListenerExample implements TestWatcher, TestExecutionExceptionHandler, AfterAllCallback {
 
     private Map<String, String> testResultStatus = new HashMap<>();
 
@@ -32,5 +35,16 @@ public class ListenerExample implements TestWatcher, AfterAllCallback {
 //                .forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
 
         testResultStatus.forEach((k, v) -> System.out.println(k + " : " + v));
+    }
+
+    //ловим каждый эксепшен в ходе выполнения тестов
+    @Override
+    public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
+        // проверяем, является ли этот эксепшен Assertion
+        if(throwable instanceof AssertionFailedError){
+            // Если да, то делаем скриншот
+            System.out.println("make screenshot");
+        }
+        throw throwable;
     }
 }
