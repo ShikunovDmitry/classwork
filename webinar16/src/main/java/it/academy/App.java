ed03637aee0780a5a4f8e5633cb9415d0f918184
Dzmitry Shykunou <dzmitry.shykunou@cantire.com>
2026-08-20 20:38:08 +0400

    maven

--- webinar16/src/main/java/it/academy/App.java
+++ webinar16/src/main/java/it/academy/App.java
@@ -1,5 +1,12 @@
 package it.academy;
 
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -7,7 +14,24 @@ package it.academy;
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        RandomStringUtils randomStringUtils = new RandomStringUtils();
+      List<String> strings = new ArrayList<>();
+//      for (int i = 0; i < 10; i++) {
+//          String randomString = randomStringUtils.nextAlphabetic(10);
+//          String caapitalizedString = StringUtils.capitalize(randomString);
+//          strings.add(caapitalizedString);
+//      }
+        for (int i = 0; i < 10; i++) {
+            strings.add(randomStringUtils.nextAlphabetic(10));
+        }
+        //strings.stream().map(s -> StringUtils.capitalize(s)).forEach(System.out::println);
+
+        List<String> result = strings.stream().map(s -> StringUtils.capitalize(s)).toList();
+        result.stream().forEach(System.out::println);
+
+
+      //strings.forEach(System.out::println);
     }
 }
